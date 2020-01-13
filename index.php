4bce2e54cd3ea2242a1b558b74be8ead7f26889a
ronstadt <ronsayan10@gmail.com>
2020-01-14 02:48:02 +0800

    up

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <!--Fontawesome ICON CDN link----------->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
 
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheetg" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
       <link rel="stylesheet" href="/resources/demos/style.css">
       <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
       <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
